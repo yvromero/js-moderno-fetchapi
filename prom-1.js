6393868407f457b6603107522fef9dcdc490dda8
yvromero <yvonne.romero@uds.edu.py>
2023-06-11 14:55:26 -0400

    Add ejercicio con callbacks

---
+++ prom-1.js
@@ -0,0 +1,38 @@
+function sumarUno( numero, callback ) {
+
+    if ( numero >= 7 ) {
+        callback('Numero muy alto');
+        return;
+    }
+
+    setTimeout( function() {
+
+        //return numero + 1;
+        callback( null, numero + 1 );
+        
+    }, 800);
+
+}
+
+sumarUno(5, function( error, nuevoValor ) {
+    if( error) {
+        console.log(error);
+        return;
+    }
+
+    sumarUno( nuevoValor, function( error, nuevoValor2 ) {
+        if( error) {
+            console.log(error);
+            return;
+        }
+
+        sumarUno( nuevoValor2, function( error, nuevoValor3 ) {
+            if( error) {
+                console.log(error);
+                return;
+            }
+        console.log(nuevoValor3);
+
+        });
+    });
+});

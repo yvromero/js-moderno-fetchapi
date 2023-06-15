0f44f24fc67c7a243adb08a676a0ee3bc251db5b
yvromero <yvonne.romero@uds.edu.py>
2023-06-15 00:29:53 -0400

    Add ej fetch methods

---
+++ fetch-2.js
@@ -0,0 +1,29 @@
+// Peticion GET 
+// https://reqres.in/api/users
+
+// fetch( 'https://reqres.in/api/users' )
+//     .then( resp => {
+
+//         resp.json().then( console.log );
+
+//     });
+
+//     fetch( 'https://reqres.in/api/users' )
+//     .then( resp => resp.json())
+//     .then( respObj => {
+//         console.log(respObj);
+//         console.log(respObj.page);
+//         console.log(respObj.per_page);
+//     });
+
+//Ejemplo de usos Fetch
+fetch( 'https://www.wikipedia.org' )
+    .then( resp => resp.text())
+    .then( html => {
+        console.log( html );
+        // document.open();
+        // document.write( html );
+        // document.close();
+
+    });
+

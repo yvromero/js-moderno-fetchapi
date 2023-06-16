a07fcfd7efdc45a9f38556fccb5a982751bafd9b
yvromero <yvonne.romero@uds.edu.py>
2023-06-15 22:21:24 -0400

    Add Tarea 1

---
+++ tarea-fetch-1.js
@@ -0,0 +1,30 @@
+// ==============================================
+// Ejercicio #1
+// ==============================================
+
+/*
+Realizar un llamado FETCH a la siguiente API
+https://swapi.dev/api
+Imprima en consola el nombre y género de la persona.
+*/
+
+// fetch( 'https://swapi.dev/api/people/1' )
+//     .then( resp => {
+
+//         if ( resp.ok ) {
+//             return resp.json()
+//         .then( persona => {
+//             console.log(persona.name)
+//             console.log(persona.gender)
+//         })
+//         } else {
+//             throw new Error("No existe el usuario");
+//         }
+
+//     })
+
+//     .catch( error => {
+//         console.log('Error en la peticion');
+//         console.log('error');
+// });
+

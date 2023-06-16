05449e9869ee1ce6d2d6bad93cc86e67937935e6
yvromero <yvonne.romero@uds.edu.py>
2023-06-15 21:22:57 -0400

    E.g leer archivos HTML

---
+++ fetch-6.js
@@ -0,0 +1,15 @@
+
+
+fetch('no-encontrado.html')
+    .then( resp => resp.text() )
+    .then( html => {
+
+        let body = document.querySelector('body');
+        body.innerHTML = html;
+
+
+    })
+    .catch( error => {
+        console.log('Error en la peticion');
+        console.log( error );
+    });
